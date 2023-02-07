2df3892ae545c0773ef2ab274c5c4ba13634376c
chukwuyemjohn <chukwuyem.j.john@gmail.com>
2023-02-07 03:56:08 +0000

    init react project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/navbar'
+import Main from './components/main'
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+    
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
